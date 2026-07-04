f0c2b852ac4e48a167a6d8bfaeeb84b3e979755c
Skanda Smaran V <103320991+skanda2271@users.noreply.github.com>
2026-07-04 19:15:12 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,9 @@
 import logging
 import os
 from datetime import datetime
+import sys
+
+from src.exception import CustomException
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
@@ -15,3 +18,10 @@ logging.basicConfig(
 
 
 )
+
+if __name__=="__main__":
+    logging.info("Logging has started")
+    try:
+        a=1/0
+    except Exception as e:
+        raise CustomException(e,sys)
