7032d173e53ee29f1764b83f1410ef674056b1f9
Skanda Smaran V <103320991+skanda2271@users.noreply.github.com>
2026-07-06 15:20:31 +0530

    issue fix

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipelines.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
